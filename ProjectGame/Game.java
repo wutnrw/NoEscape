7e80c9b8cafb45225f753818afe686f9f9562167
wutnrw <94518838+wutnrw@users.noreply.github.com>
2021-11-18 20:12:08 +0700

    Add files via upload

---
+++ ProjectGame/Game.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ProjectGame;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.net.URL;
+import javax.swing.*;
+import javax.swing.JFrame;
+/**
+ *
+ * @author Narawut
+ */
+public class Game {
+     public static void main(String[] args){
+        JFrame frame= new JFrame("No Escape");
+        frame.setContentPane(new GamePanel());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setVisible(true);
+    
+    }
+}
+
---
+++ ProjectGame/Player.java
@@ -0,0 +1,260 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ProjectGame;
+
+/**
+ *
+ * @author Narawut
+ */
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Player extends MapObject {
+	
+	private boolean flinching;
+	private long flinchTimer;
+	
+
+	// scratch
+	private boolean scratching;
+	
+	// gliding
+	private boolean gliding;
+	
+	// animations
+	private ArrayList<BufferedImage[]> sprites;
+	private final int[] numFrames = {
+		2, 8, 1, 2, 4, 2, 5
+	};
+	
+	// animation actions
+	private static final int IDLE = 0;
+	private static final int WALKING = 1;
+	private static final int JUMPING = 2;
+	private static final int FALLING = 3;
+	private static final int GLIDING = 4;
+	private static final int FIREBALL = 5;
+	private static final int SCRATCHING = 6;
+
+    public Player(TileMap tm) {
+		
+	super(tm);
+		
+		width = 20;
+		height = 20;
+		cwidth = 20;
+		cheight = 20;
+		
+		moveSpeed = 0.3;
+		maxSpeed = 1.6;
+		stopSpeed = 0.4;
+		fallSpeed = 0.15;
+		maxFallSpeed = 4.0;
+		jumpStart = -4.8;
+		stopJumpSpeed = 0.3;
+		
+		facingRight = true;
+		// load sprites
+		try {
+			
+			BufferedImage spritesheet = ImageIO.read(
+				getClass().getResourceAsStream(
+					"/Picture/red.png"
+				)
+			);
+			
+			sprites = new ArrayList<BufferedImage[]>();
+			for(int i = 0; i < 7; i++) {
+				
+				BufferedImage[] bi = new BufferedImage[numFrames[i]];
+				
+				for(int j = 0; j < numFrames[i]; j++) {
+					
+					if(i != 6) {
+						bi[j] = spritesheet.getSubimage(j * width,i * height,width,height);
+					}
+					else {
+                                            bi[j] = spritesheet.getSubimage(j * width * 2,i * height,width,height);
+					}
+					
+				}
+				
+				sprites.add(bi);
+				
+			}
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		animation = new Animation();
+		currentAction = IDLE;
+		animation.setFrames(sprites.get(IDLE));
+		animation.setDelay(400);
+		
+	}
+	
+	public void setGliding(boolean b) { 
+		gliding = b;
+	}
+	
+	private void getNextPosition() {
+		
+		// movement
+		if(left) {
+			dx -= moveSpeed;
+			if(dx < -maxSpeed) {
+				dx = -maxSpeed;
+			}
+		}
+		else if(right) {
+			dx += moveSpeed;
+			if(dx > maxSpeed) {
+				dx = maxSpeed;
+			}
+		}
+		else {
+			if(dx > 0) {
+				dx -= stopSpeed;
+				if(dx < 0) {
+					dx = 0;
+				}
+			}
+			else if(dx < 0) {
+				dx += stopSpeed;
+				if(dx > 0) {
+					dx = 0;
+				}
+			}
+		}
+		
+		// jumping
+		if(jumping && !falling) {
+			dy = jumpStart;
+			falling = true;	
+		}
+		
+		// falling
+		if(falling) {
+			
+			if(dy > 0 && gliding) dy += fallSpeed * 0.1;
+			else dy += fallSpeed;
+			
+			if(dy > 0) jumping = false;
+			if(dy < 0 && !jumping) dy += stopJumpSpeed;
+			
+			if(dy > maxFallSpeed) dy = maxFallSpeed;
+			
+		}
+		
+	}
+	
+	public void update() {
+		
+		// update position
+		getNextPosition();
+		checkTileMapCollision();
+		setPosition(xtemp, ytemp);
+		
+		// set animation
+		if(scratching) {
+			if(currentAction != SCRATCHING) {
+				currentAction = SCRATCHING;
+				animation.setFrames(sprites.get(SCRATCHING));
+				animation.setDelay(50);
+				width = 60;
+			}
+		}
+		else if(dy > 0) {
+			if(gliding) {
+				if(currentAction != GLIDING) {
+					currentAction = GLIDING;
+					animation.setFrames(sprites.get(GLIDING));
+					animation.setDelay(100);
+					width = 30;
+				}
+			}
+			else if(currentAction != FALLING) {
+				currentAction = FALLING;
+				animation.setFrames(sprites.get(FALLING));
+				animation.setDelay(100);
+				width = 30;
+			}
+		}
+		else if(dy < 0) {
+			if(currentAction != JUMPING) {
+				currentAction = JUMPING;
+				animation.setFrames(sprites.get(JUMPING));
+				animation.setDelay(-1);
+				width = 30;
+			}
+		}
+		else if(left || right) {
+			if(currentAction != WALKING) {
+				currentAction = WALKING;
+				animation.setFrames(sprites.get(WALKING));
+				animation.setDelay(40);
+				width = 30;
+			}
+		}
+		else {
+			if(currentAction != IDLE) {
+				currentAction = IDLE;
+				animation.setFrames(sprites.get(IDLE));
+				animation.setDelay(400);
+				width = 30;
+			}
+		}
+		
+		animation.update();
+		
+		// set direction
+		if(currentAction != SCRATCHING && currentAction != FIREBALL) {
+			if(right) facingRight = true;
+			if(left) facingRight = false;
+		}
+		
+	}
+	
+	public void draw(Graphics2D g) {
+		
+		setMapPosition();
+		
+		// draw player
+		if(flinching) {
+			long elapsed =
+				(System.nanoTime() - flinchTimer) / 1000000;
+			if(elapsed / 100 % 2 == 0) {
+				return;
+			}
+		}
+		
+		if(facingRight) {
+			g.drawImage(
+				animation.getImage(),
+				(int)(x + xmap - width / 2),
+				(int)(y + ymap - height / 2),
+				null
+			);
+		}
+		else {
+			g.drawImage(
+				animation.getImage(),
+				(int)(x + xmap - width / 2 + width),
+				(int)(y + ymap - height / 2),
+				-width,
+				height,
+				null
+			);
+			
+		}
+		
+	}
+	
+}
+
